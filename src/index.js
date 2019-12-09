23255ff1d0dc950a50884b122c1d6fdb17f90ed8
Toshiaki Maki <tmaki@pivotal.io>
2019-12-09 12:15:45 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const initialize = () => {
     const button = document.createElement('button');
     const response = document.createElement('pre');
-    const getUserAgent = async () => {
-        const data = await fetch('https://httpbin.org/user-agent').then(res => res.json());
-        response.innerText = data['user-agent'];
+    const onClick = async () => {
+        response.innerText = await userAgent();
     };
     button.innerText = 'Check User-Agent';
-    button.addEventListener('click', getUserAgent);
+    button.addEventListener('click', onClick);
     document.body.appendChild(button);
     document.body.appendChild(response);
 };
+
+const userAgent = () => fetch('https://httpbin.org/user-agent')
+    .then(res => res.json())
+    .then(data => data['user-agent']);
 
 document.addEventListener('DOMContentLoaded', initialize);
