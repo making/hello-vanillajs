e18bb060442102d6e760a07a156060ab185a679e
Toshiaki Maki <tmaki@pivotal.io>
2019-12-09 12:09:52 +0900

    Add Fetch sample

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const initialize = () => {
-    const element = document.createElement('h1');
-    element.innerHTML = 'Hello World!';
-    document.body.appendChild(element);
+    const button = document.createElement('button');
+    const response = document.createElement('pre');
+    const getUserAgent = async () => {
+        const data = await fetch('https://httpbin.org/user-agent').then(res => res.json());
+        response.innerText = data['user-agent'];
+    };
+    button.innerText = 'Check User-Agent';
+    button.addEventListener('click', getUserAgent);
+    document.body.appendChild(button);
+    document.body.appendChild(response);
 };
 
 document.addEventListener('DOMContentLoaded', initialize);
